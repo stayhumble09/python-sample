550bb4c71a305f01b176eb7425fe753e3c0edcaf
halland09 <pjs980720@gmail.com>
2025-05-13 10:19:53 +0900

    이미지 추가

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -1,4 +1,14 @@
 package controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/index")
 public class HomeController {
+    @GetMapping("/index")
+    public String home() {
+        return "index/index";
+    }
 }
